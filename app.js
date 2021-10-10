ed044104b9a5127f6d14a283ee40477e8f3b5dea
riefer02 <andrew.riefenstahl@gmail.com>
2021-10-10 17:24:47 -0500

    establish connection with application frontend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 const db = require('./db/index');
 
 var indexRouter = require('./routes/index');
@@ -9,6 +10,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function (req, res, next) {
+  res.json({ message: 'Hello from the backend.' });
 });
 
 module.exports = router;
