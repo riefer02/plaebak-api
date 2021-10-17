17eb7bc22a1f1beee0c74a785ec6133b937a1914
riefer02 <andrew.riefenstahl@gmail.com>
2021-10-16 19:05:17 -0500

    receive file and meta from fe successfully

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function (req, res, next) {
+router.post('/', upload.single('upload'), function (req, res, next) {
+  console.log('req.body', req.body);
+  console.log('req.file', req.file);
   res.json({ message: 'Hello from the backend.' });
 });
 
