64cbdcb7659b948ca946168afdf199ac6ab05fcc
riefer02 <andrew.riefenstahl@gmail.com>
2022-05-08 17:14:49 -0500

    add prisma to app

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
 app.use('/audio', audioRouter);
 
 module.exports = app;
--- routes/audio.js
+++ routes/audio.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+
 const fetchFile = require('../features/fetchFile');
 const uploadFile = require('../features/uploadFile');
 
@@ -9,8 +10,15 @@ router.get('/:file_name', function (req, res, next) {
   fetchFile(req.params.file_name, res);
 });
 
+router.get('/songs', (req, res, next)=>{
+  const allSongs = await prisma.songs.findMany()
+  res.json(allSongs);
+})
+
 router.post('/upload', upload.single('upload-file'), function (req, res, next) {
   uploadFile(req, res);
 });
+
+
 
 module.exports = router;
